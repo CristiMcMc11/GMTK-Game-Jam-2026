ef2f854d3095ed7f4b541b575ca22c27d8b0a328
CristiMcMc11 <crimunt0@gmail.com>
2026-07-24 13:24:24 -0700

    don't know how this didn't make it into prev commit

--- Assets/Scripts/Items/Extra Jump.cs
+++ Assets/Scripts/Items/Extra Jump.cs
@@ -19,7 +19,6 @@ public class ExtraJump : Item
     {
         ItemManager.OnJumpPressed += UseItem;
         pmScript = GameObject.Find("Player").GetComponent<PlayerMovement>();
-        GameManager.NextStage += OnStageIncrease;
     }
 
     protected override void UseItem()
